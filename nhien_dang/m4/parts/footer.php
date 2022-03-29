3d4ee76c22fa5d28bc93385e4134d15ed17d4a6e
Nhien Dang <danghaonhien@gmail.com>
2022-03-28 20:51:47 -0700

    added more contents

--- nhien_dang/m4/parts/footer.php
+++ nhien_dang/m4/parts/footer.php
@@ -1,5 +1,5 @@
   <!-- Footer -->
-  <footer>
+  <!-- <footer>
       <div class="static">
         &copy;
         <span id="copyright">
@@ -11,4 +11,40 @@
         </span>
         Nhien Dang
       </div>
-    </footer>
+    </footer> -->
+
+    <footer class="footer">
+            <!-- <div class="footer_logo-box">
+
+                <picture class="footer_logo">
+                    <source srcset="img/sunset.jpg 1x, img/sunset.jpg 2x"
+                            media="(max-width: 37.5em)">
+                    <img srcset="img/logo-green-1x.png 1x, img/logo-green-2x.png 2x" alt="Full logo" src="img/logo-green-2x.png">
+                </picture>
+
+
+                
+            </div> -->
+            <div class="grid gap">
+                <div class="col-xs-12 col-md-6">
+                    <div class="footer_navigation">
+                        <ul class="footer_list">
+                            <li class="footer_item"><a href="#" class="footer_link">Company</a></li>
+                            <li class="footer_item"><a href="#" class="footer_link">Contact us</a></li>
+                            <li class="footer_item"><a href="#" class="footer_link">Carrers</a></li>
+                            <li class="footer_item"><a href="#" class="footer_link">Privacy policy</a></li>
+                            <li class="footer_item"><a href="#" class="footer_link">Terms</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <p class="footer_copyright">
+                        Built by <a href="#" class="footer_link">Nhien Dang</a> for his class <a href="#" class="footer_link">Tech 2: Back-End </a>.
+                        Copyright &copy; by Nhien Dang. You are 100% allowed to use this webpage for both personal
+                        and commercial use, but NOT to claim it as your own design. A credit to the original author, Nhien Dang, is of course highly appreciated!
+                    </p>
+                </div>
+            </div>
+        </footer>
+
+     
--- nhien_dang/m4/parts/subMeta.php
+++ nhien_dang/m4/parts/subMeta.php
@@ -2,7 +2,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="https://use.fontawesome.com/89c4228ad7.js"></script>
-<link rel="stylesheet" href="../lib/css/styleguide.css" />
+    <link rel="stylesheet" href="../lib/css/styles.css" />
+    <link rel="stylesheet" href="../lib/css/styleguide.css" />
     <link rel="stylesheet" href="../css/storetheme.css" />
     <link rel="stylesheet" href="../lib/css/gridsystem.css" />
     <link rel="stylesheet" href="../lib/css/carousel.css" />
@@ -10,3 +11,8 @@
     <link rel="stylesheet" href="../lib/css/navigation.css" />
     <link rel="stylesheet" href="../lib/css/cards.css" />
     <link rel="stylesheet" href="../lib/css/form.css" />
+    <link rel="stylesheet" href="../lib/css/footer.css" />
+    <link rel="stylesheet" href="../lib/css/table.css" />
+    <link rel="stylesheet" href="../lib/css/utilities.css" />
+    <link rel="stylesheet" href="../lib/css/form.css" />
+    <link rel="stylesheet" href="../lib/css/hero.css" />
