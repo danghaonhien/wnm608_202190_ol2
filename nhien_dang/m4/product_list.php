67d6df518553afef5dfe6285b74fca7fb14a7613
Nhien Dang <danghaonhien@gmail.com>
2022-05-09 11:17:57 -0700

    updated product list

--- nhien_dang/m4/product_list.php
+++ nhien_dang/m4/product_list.php
@@ -9,39 +9,74 @@ include_once "parts/templates.php";
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Product List</title>
-
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 	<?php include "parts/meta.php"; ?>
 	<script src="lib/css/js/functions.js"></script>
 	<script src="js/templates.js"></script>
-	<script>
+	<script src="js/product_list.js"></script>
+
+	<!-- <script>
 		query({type:'products_all'}).then(d=>{
 			$(".productlist").html(listItemTemplate(d.result))
 		});
-	</script>
+	</script> -->
 </head>
 
 <body>
-<?php include "parts/navbar.php"; ?>
+	<?php include "parts/navbar.php"; ?>
 	<div class="styleguidecontainer gapContainer">
 		<div class="">
 			<h2>NFTs</h2>
-			<!-- <ul>
-				<li><a href="product_item.php?id=1">Product One</a></li>
-				<li><a href="product_item.php?id=2">Product Two</a></li>
-				<li><a href="product_item.php?id=3">Product Three</a></li>
-				<li><a href="product_item.php?id=4">Product Four</a></li>
-			</ul> -->
 			<br />
 			<div class="form-control">
-				<form class="hotdog light" id="product-search"> <input type="search" placeholder="Search Products"> </form>
+				<form class="hotdog " id="product-search">
+					<input type="search" placeholder="Search Products">
+				</form>
+			</div>
+
+
+
+			<div class="form-control">
+				<div class="card">
+					<div class="flex-stretch">
+
+						<!-- <div class="flex-stretch display-inline-flex"> -->
+
+
+						<div class="flex-none">
+							<button data-filter="category" data-value="" type="button" class="form-button">All</button>
+						</div>
+						<!-- <div class="flex-none">
+                                <button data-filter="category" data-value="women" type="button" class="form-button">WOMEN</button>
+                            </div>
+                              <div class="flex-none">
+                                <button data-filter="category" data-value="men" type="button" class="form-button">MEN</button>
+                            </div> -->
+						<!-- </div>   -->
+
+
+						<div class="flex-none">
+							<div class="form-select">
+								<select class="js-sort">
+									<option value="1">PRICE LOW TO HIGH </option>
+									<option value="2">PRICE HIGH TO LOW</option>
+
+								</select>
+							</div>
+						</div>
+					</div>
+
+				</div>
 
 			</div>
-			<div class='productlist grid gap'></div>
-		
-	</div>
 
-	<!-- Footer -->
-	<?php include "parts/footer.php"; ?>
+
+
+			<div class='productlist grid gap'></div>
+		</div>
+
+		<!-- Footer -->
+		<?php include "parts/footer.php"; ?>
 
 </body>
 
